11c820885f04eabaaea4eab7cbe2e7859e88a43a
SaffronGamer <saffrongamer167@gmail.com>
2025-01-24 11:01:28 -0700

    Totally not a bit coin miner

--- GitForFun/Program.cs
+++ GitForFun/Program.cs
@@ -2,8 +2,17 @@
 
 public class Program
 {
+    // This should be written in Rust... or javascript... or mips...
     static void Main()
     {
         //Comment
+        while (true) {
+            MineBitCoin(); // We will use all available CPU cycles to mine a single bitcoin HAHAHAHA!!!
+        }
+    }
+
+    public static int MineBitCoin() {
+        int ans = 69420;
+        return ans;
     }
 }
